061c8d6070713e742bfb2bdcedc105e975308487
SombrunJoe <34570095+SombrunJoe@users.noreply.github.com>
2020-11-08 21:07:51 +0100

    css part 1.1

--- src/pages/Profil.tsx
+++ src/pages/Profil.tsx
@@ -1,22 +1,24 @@
-import {
-  IonList,
-  IonItem,
-  IonLabel,
-  IonInput,
-  IonToggle,
-  IonRadio,
-  IonCheckbox,
-  IonItemSliding,
-  IonItemOption,
-  IonItemOptions,
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar
+import { 
+  IonList, 
+  IonItem, 
+  IonLabel, 
+  IonInput, 
+  IonToggle, 
+  IonRadio, 
+  IonCheckbox, 
+  IonItemSliding, 
+  IonItemOption, 
+  IonItemOptions, 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
 } from '@ionic/react';
 import React from 'react';
+// import ListPage from '../components/ListPage';
 import Name from '../components/profil'
+import './Profil.css';
 
 const Profil: React.FC = () => {
   return (
